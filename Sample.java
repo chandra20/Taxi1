82755286c4871adeb6bc578d8cee8072226eca7f
thenetworkjob <thenetworkjob@gmail.com>
2012-05-28 13:09:27 +0530

    first commit

---
+++ Sample.java
@@ -0,0 +1,40 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lumiin
+ * Date: 5/24/12
+ * Time: 12:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.io.*;
+import java.sql.*;
+public class Sample {
+   public static void main(String ar[])
+   {
+       try
+       {
+           Class.forName("com.mysql.jdbc.Driver");
+           String url="jdbc:mysql://localhost:3306/test";
+           Connection con=DriverManager.getConnection(url,"root","root");
+           System.out.println("connected");
+           Statement stmt = con.createStatement();
+           ResultSet rs = stmt.executeQuery("select * from cute");
+           System.out.println("selected");
+           while(rs.next())
+           {
+               String s1=rs.getString("Ename");
+               String s2=rs.getString("Email");
+               String s3=rs.getString("Password");
+               String s4=rs.getString("Address");
+               System.out.println(s1);
+               System.out.println(s2);
+               System.out.println(s3);
+               System.out.println(s4);
+           }
+       }
+       catch(Exception e)
+       {
+           System.out.println(e);
+       }
+   }
+
+}
---
+++ Simple.java
@@ -0,0 +1,43 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: lumiin
+ * Date: 5/24/12
+ * Time: 11:44 AM
+ * To change this template use File | Settings | File Templates.
+ */
+import java.io.*;
+import java.sql.*;
+public class Simple {
+    public static void main(String ar[])
+    {
+        try
+        {
+            Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+            Connection con=DriverManager.getConnection("jdbc:odbc:test");
+            System.out.println("Connected");
+            Statement st=con.createStatement();
+            ResultSet rs=st.executeQuery("select * from cute");
+            while(rs.next())
+            {
+                String s1=rs.getString("Ename");
+                String s2=rs.getString("Email");
+                String s3=rs.getString("Password");
+                String s4=rs.getString("Address");
+                System.out.print(s1);
+                System.out.print("\t");
+                System.out.print(s2);
+                System.out.print("\t");
+                System.out.print(s3);
+                System.out.print("\t");
+                System.out.print(s4);
+                System.out.print("\n");
+            }
+        }
+        catch(Exception e)
+        {
+            System.out.println(e);
+        }
+
+    }
+
+}
